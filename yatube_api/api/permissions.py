8cc06357e3b22fe4eb31aac7694b99375fb7babf
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-08-05 11:40:56 +0100

    1st iteration review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,6 +5,13 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     message = 'Only Author is permitted'
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
